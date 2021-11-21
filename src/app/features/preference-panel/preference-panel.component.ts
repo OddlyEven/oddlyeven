27d768c8af67f8f038488f2ae1165dd4e517c864
Sean Street <OddlyEven@users.noreply.github.com>
2021-11-21 09:51:57 -0600

    Adding missing files

---
+++ src/app/features/preference-panel/preference-panel.component.ts
@@ -0,0 +1,42 @@
+import { AfterViewInit, Component, EventEmitter, Input, Output } from '@angular/core';
+import { Shapes } from 'src/app/core/constants/shapes';
+import { ShapePreferences } from 'src/app/core/interfaces/shape-preferences';
+
+@Component({
+  selector: 'app-preference-panel',
+  templateUrl: './preference-panel.component.html',
+  styleUrls: ['./preference-panel.component.scss']
+})
+export class PreferencePanelComponent implements AfterViewInit {
+  @Input() preferences!: ShapePreferences;
+  @Input() isVisible = true;
+  @Output() preferenceChange = new EventEmitter<ShapePreferences>();
+
+  public shapes = ['diamond', 'triangle'];
+  public sizeRange = [...Array(13).keys()].map((key) => (key + 3) * 10);
+  public selectedShape!: Shapes;
+  public selectedSize!: number;
+
+  public ngAfterViewInit(): void {
+    setTimeout(() => this.initFields());
+  }
+
+  public shapeSelected(evt: any): void {
+    this.preferences = { ...this.preferences, shape: evt.target.value };
+    this.preferencesChanged();
+  }
+
+  public sizeSelected(evt: any): void {
+    this.preferences = { ...this.preferences, size: Number(evt.target.value) };
+    this.preferencesChanged();
+  }
+
+  public preferencesChanged(): void {
+    this.preferenceChange.emit(this.preferences);
+  }
+
+  private initFields(): void {
+    this.selectedShape = this.preferences.shape;
+    this.selectedSize = this.preferences.size;
+  }
+}
