c59475e0f1571743178b47f390ca5d3ec340edb4
Sean Street <sean.street@protonmail.com>
2021-11-23 21:06:03 -0600

    Added animated banner

--- src/app/features/preference-panel/preference-panel.component.ts
+++ src/app/features/preference-panel/preference-panel.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Enum, Shapes } from 'src/app/core/constants/shapes';
 import { ShapePreferences } from 'src/app/core/interfaces/shape-preferences';
 
@@ -7,19 +7,14 @@ import { ShapePreferences } from 'src/app/core/interfaces/shape-preferences';
   templateUrl: './preference-panel.component.html',
   styleUrls: ['./preference-panel.component.scss']
 })
-export class PreferencePanelComponent implements AfterViewInit {
+export class PreferencePanelComponent {
   @Input() preferences!: ShapePreferences;
   @Input() isVisible = true;
+
   @Output() preferenceChange = new EventEmitter<ShapePreferences>();
 
   public shapes = Enum.names(Shapes);
   public sizeRange = [...Array(13).keys()].map((key) => (key + 3) * 10);
-  public selectedShape!: Shapes;
-  public selectedSize!: number;
-
-  public ngAfterViewInit(): void {
-    setTimeout(() => this.initFields());
-  }
 
   public shapeSelected(evt: any): void {
     this.preferences = { ...this.preferences, shape: evt.target.value };
@@ -33,10 +28,5 @@ export class PreferencePanelComponent implements AfterViewInit {
 
   public preferencesChanged(): void {
     this.preferenceChange.emit(this.preferences);
-  }
-
-  private initFields(): void {
-    this.selectedShape = this.preferences.shape;
-    this.selectedSize = this.preferences.size;
   }
 }
